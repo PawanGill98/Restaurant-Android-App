617a80a686fab01b7ac7d22d5cb3973f9d143f01
roger <pla90@sfu.ca>
2020-03-03 19:47:11 -0800

    Added reading from restaurants_itr.csv file
    Completed reading and storing of restaurant attributes to restaurant list.

--- app/src/main/java/me/cmpt276/restaurantinspector/MainActivity.java
+++ app/src/main/java/me/cmpt276/restaurantinspector/MainActivity.java
@@ -3,6 +3,17 @@ package me.cmpt276.restaurantinspector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.cmpt276.restaurantinspector.Model.Restaurant;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readRestaurantData();
+    }
+
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    private void readRestaurantData() {
+        InputStream is = getResources().openRawResource(R.raw.restaurants_itr1);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+        try {
+            // Step over headers
+            reader.readLine();
+
+            while ((line = reader.readLine()) != null) {
+                Log.d("MyActivity", "Line: " + line);
+                // Split by ','
+                String[] tokens = line.split(",");
+
+                // Read the data
+                Restaurant restaurant = new Restaurant();
+                restaurant.setTrackingNumber(tokens[0]);
+                restaurant.setName(tokens[1]);
+                restaurant.setAddress(tokens[2]);
+                restaurant.setCity(tokens[3]);
+                restaurant.setFacilityType(tokens[4]);
+                restaurant.setLatitude(Double.parseDouble(tokens[5]));
+                restaurant.setLongitude(Double.parseDouble(tokens[6]));
+                restaurants.add(restaurant);
+                Log.d("MyActivity", "Just created: " + restaurant);
+            }
+        } catch (IOException e) {
+            Log.wtf("MyActivity", "Error reading data file on line " + line, e);
+            e.printStackTrace();
+        }
     }
 }
